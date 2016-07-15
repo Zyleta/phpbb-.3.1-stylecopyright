2a684a373264ef77b9a6076f96122c97d351eab6
Zyleta <mateusz.zyleta@gmail.com>
2016-07-15 16:36:42 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# phpbb-.3.1-stylecopyright
-An extension for phpBB 3.1 that add a copyright for style, like a name of style, parent style, author
+# Style copyright in footer extension based on Copyright in footer extension by dmzx
+
+## Install
+
+1. Download the latest release.
+2. Unzip the downloaded release, and change the name of the folder to `stylecopyright`.
+3. In the `ext` directory of your phpBB board, create a new directory named `zyleta` (if it does not already exist).
+4. Copy the `stylecopyright` folder to `/ext/zyleta/` (if done correctly, you'll have the main extension class at (your forum root)/ext/zyleta/stylecopyright/composer.json).
+5. Navigate in the ACP to `Customise -> Manage extensions`.
+6. Look for `Style copyright in footer` under the Disabled Extensions list, and click its `Enable` link.
+
+## Uninstall
+
+1. Navigate in the ACP to `Customise -> Extension Management -> Extensions`.
+2. Look for `Style copyright in footer` under the Enabled Extensions list, and click its `Disable` link.
+3. To permanently uninstall, click `Delete Data` and then delete the `/ext/zyleta/stylecopyright` folder.
+
+## License
+[GNU General Public License v2](http://opensource.org/licenses/GPL-2.0)
---
+++ event/listener.php
@@ -0,0 +1,82 @@
+<?php
+/**
+*
+* @package phpBB Extension - Style copyright in footer
+* @copyright (c) 2016 Zyleta
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace zyleta\stylecopyright\event;
+
+/**
+* @ignore
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+	/** @var \phpbb\template\template 	*/
+	protected $template;
+	/** @var \phpbb\user */
+	protected $user;
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+	
+	/**
+	* Constructor
+	*
+	* @param \phpbb\template\template				$template
+	* @param \phpbb\user 							$user
+	* @param \phpbb\db\driver\driver_interface 		$db
+	*
+	*/
+	public function __construct(\phpbb\template\template $template, \phpbb\user $user)
+	{
+		global $db;
+		$this->template = $template;
+		$this->user = $user;
+		$this->db = $db;
+	}
+	
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.user_setup'		=> 'load_language_on_setup',
+			'core.page_footer'		=> 'page_footer',
+		);
+	}
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'zyleta/stylecopyright',
+			'lang_set' => 'common',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	protected function get_copyright()
+	{
+		$sql = 'SELECT *
+			FROM ' . STYLES_TABLE . '
+			WHERE style_id = ' . (int) $this->user->data['user_style'];
+		$result = $this->db->sql_query($sql);
+		$row = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+		return $row;
+	}
+
+	public function page_footer($event)
+	{
+		$this->template->assign_vars(array(
+			'L_STYLE_COPYRIGHT'			=> $this->get_copyright()['style_copyright'],
+			'L_STYLE_NAME'				=> $this->get_copyright()['style_name'],
+			'L_PARENT_STYLE'			=> $this->get_copyright()['style_parent_tree'],
+		));
+	}
+}
